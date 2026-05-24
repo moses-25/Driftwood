bf1b71cedbba8cbfd1972ef04331381201f2fb34
Mosesotieno-ai <mosesotieno8363@gmail.com>
2026-05-25 00:58:06 +0300

    feat: Update contact information in Footer component

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -19,7 +19,7 @@ const contactInfo = [
         <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
       </svg>
     ),
-    text: '123 Coffee Street, Brewville, CA 90210',
+    text: 'NAIROBI, KENYA',
     href: 'https://maps.google.com/?q=123+Coffee+Street+Brewville+CA+90210',
   },
   {
@@ -28,8 +28,8 @@ const contactInfo = [
         <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
       </svg>
     ),
-    text: '(555) 123-4567',
-    href: 'tel:+15551234567',
+    text: '(254) 757-268-162',
+    href: 'tel:+254757268162',
   },
   {
     icon: (
